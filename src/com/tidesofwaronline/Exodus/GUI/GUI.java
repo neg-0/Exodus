cd61f7a52ca5d4e5701204c638e4aaeec518e55e
TidesofWarOnline <mastac@gmail.com>
2013-03-14 20:02:26 -0500

    More work on the GUI

--- src/com/tidesofwaronline/Exodus/GUI/GUI.java
+++ src/com/tidesofwaronline/Exodus/GUI/GUI.java
@@ -1,6 +1,7 @@
 package com.tidesofwaronline.Exodus.GUI;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class GUI extends JFrame {
@@ -14,6 +15,8 @@ public class GUI extends JFrame {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 		this.setLocationRelativeTo(null);
+		
+		this.add(new Panel());
 	}
 	
 	public class Panel extends JPanel {
@@ -21,6 +24,11 @@ public class GUI extends JFrame {
 		private static final long serialVersionUID = -6020268014214860857L;
 		
 		public Panel() {
+			this.setSize(128, 768);
+			this.setLocation(0, 0);
+			this.setVisible(true);
+			
+			this.add(new JLabel("Control Panel"));
 		}
 		
 	}
