1e53805bcd4c2371bbd00848bae95fa1343d1148
TidesofWarOnline <mastac@gmail.com>
2013-03-14 20:32:43 -0500

    GUI Changes

--- src/com/tidesofwaronline/Exodus/GUI/GUI.java
+++ src/com/tidesofwaronline/Exodus/GUI/GUI.java
@@ -9,27 +9,27 @@ public class GUI extends JFrame {
 	private static final long serialVersionUID = -382704924909844493L;
 
 	public GUI() {
-		
+
 		this.setSize(1024, 768);
 		this.setTitle("Exodus GUI");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 		this.setLocationRelativeTo(null);
-		
+
 		this.add(new Panel());
 	}
-	
+
 	public class Panel extends JPanel {
 
 		private static final long serialVersionUID = -6020268014214860857L;
-		
+
 		public Panel() {
 			this.setSize(128, 768);
 			this.setLocation(0, 0);
 			this.setVisible(true);
-			
+
 			this.add(new JLabel("Control Panel"));
 		}
-		
+
 	}
 }
