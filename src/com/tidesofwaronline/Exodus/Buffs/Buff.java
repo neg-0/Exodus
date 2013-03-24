baeff162bdf606bc68f459e33fa5d2f9ce3cda14
TidesofWarOnline <mastac@gmail.com>
2013-03-23 21:49:29 -0500

    Added needed methods

--- src/com/tidesofwaronline/Exodus/Buffs/Buff.java
+++ src/com/tidesofwaronline/Exodus/Buffs/Buff.java
@@ -1,7 +1,13 @@
 package com.tidesofwaronline.Exodus.Buffs;
 
+import org.bukkit.entity.LivingEntity;
+
+import com.tidesofwaronline.Exodus.Player.ExoPlayer;
+
 public abstract class Buff {
 	
-	public abstract void onHit();
+	public abstract void onDamage(ExoPlayer player, LivingEntity entity); //Called when the player is attacked by a LivingEntity
+	public abstract void onHit(ExoPlayer player, LivingEntity entity);   //Called when the player attacks a LivingEntity
+	public abstract void onTick(ExoPlayer player);     //Called every tick
 
 }
