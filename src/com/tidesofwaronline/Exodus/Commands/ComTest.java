79bfcad2336b9a116908ce2f0e0a67be37193e48
TidesofWarOnline <mastac@gmail.com>
2013-07-11 14:48:02 -0500

    Test updates

--- src/com/tidesofwaronline/Exodus/Commands/ComTest.java
+++ src/com/tidesofwaronline/Exodus/Commands/ComTest.java
@@ -1,16 +1,11 @@
 package com.tidesofwaronline.Exodus.Commands;
 
-import java.util.HashMap;
-
 import com.tidesofwaronline.Exodus.Player.ExoPlayer;
-import com.tidesofwaronline.Exodus.Races.Races.Race;
 
 public class ComTest extends Command {
 
 	public ComTest(ExoPlayer player, String[] args) {
 		
-		HashMap<Race, Integer> races = player.getRacesByRep();
-		
-		player.getPlayer().sendMessage("" + races.size());
+		player.getPlayer().sendMessage(player.getPlayer().getItemInHand().toString());
 	}
 }
