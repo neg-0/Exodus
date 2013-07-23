2b6e7ec75f46a9d1c5a47bc90fef0dfc99e1ea2f
TidesofWarOnline <mastac@gmail.com>
2013-07-22 23:32:36 -0500

    Added reflection!

--- src/com/tidesofwaronline/Exodus/Commands/ComDBEBlockCommand.java
+++ src/com/tidesofwaronline/Exodus/Commands/ComDBEBlockCommand.java
@@ -1,19 +1,63 @@
 package com.tidesofwaronline.Exodus.Commands;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+import org.bukkit.entity.Player;
+
+import com.tidesofwaronline.Exodus.DungeonBlocks.DungeonBlockCommand;
 import com.tidesofwaronline.Exodus.Player.ExoPlayer;
 
 public class ComDBEBlockCommand {
 
 	public ComDBEBlockCommand(ExoPlayer exop, String message) {
-		if (message.equalsIgnoreCase("delete")) {
-			exop.editingBlock.delete();
-			exop.getPlayer().sendMessage(exop.editingBlock.toString() + " has been deleted and removed.");
-			exop.editingBlock = null;
-		} else if (message.equalsIgnoreCase("exit")) {
-			exop.getPlayer().sendMessage("No longer editing " + exop.editingBlock.toString());
-			exop.editingBlock = null;
+		
+		Player p = exop.getPlayer();
+		
+		String[] split = message.split(" ");
+		String command = split[0];
+		String[] arguments = Arrays.copyOfRange(split, 1, split.length);
+		
+		Method[] methods = exop.getEditingBlock().getClass().getMethods();
+
+		for (Method m : methods) {
+			if (m.isAnnotationPresent(DungeonBlockCommand.class) && m.getName().equalsIgnoreCase(command)) {
+				try {
+					
+					Object o;
+					
+					if (m.getParameterTypes().length > 0) {
+						o = m.invoke(exop.getEditingBlock(), new Object[] { arguments });
+					} else {
+						o = m.invoke(exop.getEditingBlock());
+					}
+										
+					if (m.getReturnType() == String.class) {
+						p.sendMessage(o.toString());
+					} else if (m.getReturnType() == boolean.class && Boolean.parseBoolean(o.toString())) {
+						p.sendMessage("Done!");
+					}
+					
+					return;
+				} catch (IllegalArgumentException e) {
+					p.sendMessage("Invalid number of arguments");
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		if (command.equalsIgnoreCase("exit")) {
+			p.sendMessage("No longer editing " + exop.getEditingBlock().toString());
+			exop.setEditingBlock(null);
 		} else {
-			exop.getPlayer().sendMessage("Command " + message + " not found!");
+			p.sendMessage("§7Command \"" + command + "\" not valid!");
 		}
 	}
 
