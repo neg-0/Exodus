94aada6da00bf0341ae5a97843235f90129f2077
TidesofWarOnline <mastac@gmail.com>
2013-07-23 23:37:01 -0500

    Added unlink, added reflection for placement of Dungeon Blocks

--- src/com/tidesofwaronline/Exodus/DungeonBlocks/DungeonBlock.java
+++ src/com/tidesofwaronline/Exodus/DungeonBlocks/DungeonBlock.java
@@ -1,8 +1,10 @@
 package com.tidesofwaronline.Exodus.DungeonBlocks;
 
 import java.beans.Introspector;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -27,23 +29,7 @@ public abstract class DungeonBlock {
 	Location loc;
 	int ID;
 	int IDcount = 1;
-	
-	//public static final DungeonBlock TOGGLE_SWITCH
-	//public static final DungeonBlock REPEATER
-	//public static final DungeonBlock REDSTONE_SWITCH = new RedstoneSwitch();
-	//public static final DungeonBlock CLICK_TRIGGER
-	public static final DungeonBlock PROXIMITY = new ProximityTrigger();
-	//public static final DungeonBlock TIMER
-	public static final DungeonBlock ENTITY_SPAWNER = new EntitySpawner();
-	//public static final DungeonBlock ENTITY_SPAWNER
-	//public static final DungeonBlock MOB_TOSSER
-	public static final DungeonBlock INVENTORY_EDITOR = new InventoryEditor();
-	//public static final DungeonBlock TAKE_ITEM
-	//public static final DungeonBlock CAVEIN
-	//public static final DungeonBlock LIGHTNING
-	//public static final DungeonBlock EXPLOSION
-	//public static final DungeonBlock PHYSICS
-	
+
 	public void onTrigger() {
 	}
 	public void onClickBlock(ExoPlayer exodusPlayer, Block clickedBlock, Action action) {
@@ -52,7 +38,6 @@ public abstract class DungeonBlock {
 	}
 	
 	public DungeonBlock() {
-		
 	}
 	
 	public DungeonBlock(Location loc) {
@@ -122,18 +107,55 @@ public abstract class DungeonBlock {
 		return DBRegistry.get(b.getLocation());
 	}
 	
+	public static DungeonBlock getDungeonBlock(String s) {
+		for (DungeonBlock d : DBRegistry.values()) {
+			if (d.toString().equalsIgnoreCase(s)) {
+				return d;
+			}
+		}
+		return null;
+	}
+	
 	public static boolean isDungeonBlock(Location loc) {
 		return DBRegistry.containsKey(loc);
 	}
 
-	public static void placeBlock(ExoPlayer exoPlayer, ItemStack block, Location location) {
-		exoPlayer.getPlayer().sendMessage("Placed a " + block.getItemMeta().getDisplayName() + ".");
+	public static void placeBlock(ExoPlayer exoPlayer, ItemStack block, Location location) {		
+		try {
+			Class<?> c = Class.forName(DungeonBlock.class.getPackage().getName() + "." + block.getItemMeta().getDisplayName().replaceAll(" ", ""));
+			if (c.getAnnotation(DungeonBlockInfo.class) != null) {
+				DungeonBlock db = (DungeonBlock) c.getConstructor(Location.class).newInstance(location);
+				exoPlayer.getPlayer().sendMessage("Placed §3" + db + "§f.");
+			} else {
+				exoPlayer.getPlayer().sendMessage("This Dungeon Block is improperly configured: Missing Annotation.");
+			}
+		} catch (ClassNotFoundException e) {
+			exoPlayer.getPlayer().sendMessage("This is not a valid Dungeon Block.");
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		if (block.getItemMeta().getDisplayName().equals("Click Trigger")) {
+		/*if (block.getItemMeta().getDisplayName().equals("Click Trigger")) {
 			new ClickTrigger(location);
 		} else if (block.getItemMeta().getDisplayName().equals("Entity Spawner")) {
 			new EntitySpawner(location);
-		}
+		}*/
 	}
 
 	public static void breakBlockEvent(ExoPlayer exodusPlayer, Block block) {
@@ -164,10 +186,10 @@ public abstract class DungeonBlock {
 				} else if (action == Action.RIGHT_CLICK_BLOCK && p.isSneaking() == false) {
 					//Right Click
 					if (exodusPlayer.getEditingBlock() != null && exodusPlayer.getEditingBlock().equals(cb)) {
-						p.sendMessage("No longer editing " + cb.toString());
+						p.sendMessage("No longer editing " + cb);
 						exodusPlayer.setEditingBlock(null);
 					} else {
-						p.sendMessage("Editing " + cb.toString() + ". Type §ehelp §fif you don't know what you're doing!");
+						p.sendMessage("Editing §3" + cb + "§f. Type §ehelp §fif you don't know what you're doing!");
 						exodusPlayer.setEditingBlock(cb);
 					}
 					
@@ -178,7 +200,7 @@ public abstract class DungeonBlock {
 						p.sendMessage("Block Deselected");
 					} else if (sbLoc == null || !sbLoc.equals(cbLoc)) {
 						exodusPlayer.setSelectedBlock(cb);
-						p.sendMessage("Selected block: " + cb.getName());
+						p.sendMessage("Selected block: §3" + cb);
 					}
 					
 				} else if (action == Action.RIGHT_CLICK_BLOCK && p.isSneaking() == true) {
@@ -192,7 +214,7 @@ public abstract class DungeonBlock {
 						p.sendMessage("Link broken!");
 					} else {
 						sb.addLinkedBlock(cb);
-						p.sendMessage("Link created! " + sb.getName() + " -> " + cb.getName());
+						p.sendMessage("Link created! " + sb + " -> " + cb);
 					}
 				}
 			}
@@ -279,11 +301,11 @@ public abstract class DungeonBlock {
 	
 	@Override
 	public String toString() {
-		return this.getName() + " " + this.getID();
+		return "§3" + this.getName() + " " + this.getID() + "§f";
 	}
 	
-	@DungeonBlockCommand(example = "", syntax = "delete", description = "Deletes the selected Dungeon Block.")
-	public String delete() {
+	@DungeonBlockCommand(example = "delete; delete Entity Spawner 4; delete all", syntax = "delete <all, Dungeon Block>", description = "Deletes the selected Dungeon Block, specified Dungeon Block(s), or all DungeonBlocks.")
+	public String delete(CommandInfo ci) {
 		this.getBlock().setTypeId(0);
 		for (DungeonBlock d : this.getLinkedFromBlocks()) {
 			d.removeLinkedBlock(this);
@@ -296,7 +318,8 @@ public abstract class DungeonBlock {
 
 		}
 		
-		return this.toString() + " has been deleted and removed.";
+		ci.getExoPlayer().setEditingBlock(null);
+		return "§3" + this.toString() + "§f has been deleted and removed. No longer editing §3" + this.toString() + "§f.";
 	}
 	
 	@DungeonBlockCommand(example = "", syntax = "commands", description = "Returns a list of commands for the selected Dungeon Block.")
@@ -324,13 +347,14 @@ public abstract class DungeonBlock {
 	public List<String> help(CommandInfo ci) {
 		List<String> output = new ArrayList<String>();
 		if (ci.getArguments().length == 0) {
-			output.add( "This is a");
+			String s = "This is a";
 			char letter = this.getName().toLowerCase().toCharArray()[0];
 			if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
-				output.add("n");
+				s += "n";
 			}
-			output.add("§3" + this.getName() + "§f. It " + Introspector.decapitalize(this.description(null)) +
-			" To see a list of §ecommands §fyou can use on this block, simply type §ecommands§f.");
+			s += " §3" + this.getName() + "§f. It " + Introspector.decapitalize(this.description(null)) +
+			" To execute a §ecommand §f, type it into the chat box. For a list of §ecommands §f, type §ecommands §f.";
+			output.add(s);
 		} else {
 			for (Method m : this.getClass().getMethods()) {
 				if (m.isAnnotationPresent(DungeonBlockCommand.class) && m.getName().equalsIgnoreCase(ci.getArguments()[0])) {
@@ -339,7 +363,6 @@ public abstract class DungeonBlock {
 					if (!dbc.description().isEmpty()) {
 						s += " " + Introspector.decapitalize(dbc.description());
 					}
-					s += ".";
 					
 					output.add(s);
 					
@@ -429,5 +452,77 @@ public abstract class DungeonBlock {
 			}
 		}
 		return "You must specify a command to show examples.";
+	}
+	
+	@DungeonBlockCommand(description = "Creates a link to or from another Dungeon Block.", example = "link to Entity Spawner 1", syntax = "link <to/from> DungeonBlock, DungeonBlock, DungeonBlock...")
+	public String link(CommandInfo ci) {
+		if (ci.getArguments().length < 2) {
+			return "Invalid Arguments. See syntax and/or example.";
+		}
+		
+		List<DungeonBlock> dungeonBlocks = new ArrayList<DungeonBlock>();
+		String[] dungeonBlockStrings = Joiner.on(" ").join(Arrays.copyOfRange(ci.getArguments(), 1, ci.getArguments().length)).split(",");
+		
+		for (String s : dungeonBlockStrings) {
+			DungeonBlock db = getDungeonBlock(s.trim());
+			if (db != null) {
+				dungeonBlocks.add(db);
+			}
+		}
+		
+		if (dungeonBlocks.isEmpty()) {
+			return "No valid Dungeon Blocks found.";
+		}
+		
+		if (ci.getArguments()[0].equalsIgnoreCase("to")) {
+			for (DungeonBlock db : dungeonBlocks) {
+				this.addLinkedBlock(db);
+			}
+			return "Linked this Dungeon Block to §3" + Joiner.on("§f, §3").join(dungeonBlocks);
+		} else if (ci.getArguments()[0].equalsIgnoreCase("from")) {
+			for (DungeonBlock db : dungeonBlocks) {
+				db.addLinkedBlock(this);
+			}
+			return "Linked §3" + Joiner.on("§f, §3").join(dungeonBlocks) + "§f to this Dungeon Block.";
+		}
+		return "";
+	}
+	
+	@DungeonBlockCommand(description = "Removes a link to or from another Dungeon Block.", example = "unlink Entity Spawner 1", syntax = "unlink DungeonBlock, DungeonBlock, DungeonBlock...")
+	public String unlink(CommandInfo ci) {
+		if (ci.getArguments().length < 1) {
+			return "Invalid Arguments. See syntax and/or example.";
+		}
+		
+		if (ci.getArguments()[0].equalsIgnoreCase("all")) {
+			for (DungeonBlock db : this.getLinkedBlocks()) {
+				this.removeLinkedBlock(db);
+			}
+			
+			for (DungeonBlock db : this.getLinkedFromBlocks()) {
+				db.removeLinkedBlock(this);
+			}
+		}
+		
+		List<DungeonBlock> dungeonBlocks = new ArrayList<DungeonBlock>();
+		String[] dungeonBlockStrings = Joiner.on(" ").join(Arrays.copyOfRange(ci.getArguments(), 1, ci.getArguments().length)).split(",");
+		
+		for (String s : dungeonBlockStrings) {
+			DungeonBlock db = getDungeonBlock(s.trim());
+			if (db != null) {
+				dungeonBlocks.add(db);
+			}
+		}
+		
+		if (dungeonBlocks.isEmpty()) {
+			return "No valid Dungeon Blocks found.";
+		}
+		
+		for (DungeonBlock db : dungeonBlocks) {
+			this.removeLinkedBlock(db);
+			db.removeLinkedBlock(this);
+		}
+		return "Unlinked this Dungeon Block from §3" + Joiner.on("§f, §3").join(dungeonBlocks);
+
 	}
 }
