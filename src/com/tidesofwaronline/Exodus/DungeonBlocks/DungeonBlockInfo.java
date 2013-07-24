68d57053ed359b2399885ee830199c8c55d482ad
TidesofWarOnline <mastac@gmail.com>
2013-07-23 23:34:39 -0500

    Cleanup

--- src/com/tidesofwaronline/Exodus/DungeonBlocks/DungeonBlockInfo.java
+++ src/com/tidesofwaronline/Exodus/DungeonBlocks/DungeonBlockInfo.java
@@ -5,8 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(value = RetentionPolicy.RUNTIME)
-@Target(value = ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface DungeonBlockInfo {
 
 	public String name();
