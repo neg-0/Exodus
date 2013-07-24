212d42aa6c04a44cf8a74157a6f43d5681e5c340
TidesofWarOnline <mastac@gmail.com>
2013-07-23 23:35:51 -0500

    More efficient code

--- src/com/tidesofwaronline/Exodus/DungeonBlocks/DBInventory.java
+++ src/com/tidesofwaronline/Exodus/DungeonBlocks/DBInventory.java
@@ -16,30 +16,27 @@ public class DBInventory {
 	public DBInventory () {
 		
 		//Tools
-		inv.setItem(0, new DBInventoryIcon(Material.BLAZE_ROD, "Info Tool", "Displays info on a block.", "Left click for block info.", "Right click to Edit a block.", "Shift-Left Click to select a block.", "Shift-Right Click to link a block."));
+		inv.setItem(0, new DBInventoryIcon(InfoTool.class));
 		
 		//Logic Blocks
-		inv.setItem(2, new DBInventoryIcon(Material.GOLD_BLOCK, "Dungeon Settings", "§8Dungeon Block", "Displays general dungeon information."));
-		inv.setItem(11, new DBInventoryIcon(Material.PUMPKIN, "Toggle Switch", "§8Dungeon Block"));
-		inv.setItem(20, new DBInventoryIcon(Material.EMERALD_BLOCK, "Repeater", "§8Dungeon Block"));
-		inv.setItem(29, new DBInventoryIcon(Material.BRICK, "Redstone Switch", "§8Dungeon Block"));
+		//inv.setItem(11, new DBInventoryIcon(Material.PUMPKIN, "Toggle Switch", "§8Dungeon Block"));
+		//inv.setItem(20, new DBInventoryIcon(Material.EMERALD_BLOCK, "Repeater", "§8Dungeon Block"));
+		inv.setItem(29, new DBInventoryIcon(RedstoneSwitch.class));
 		
 		//Trigger Blocks
-		inv.setItem(3, new DBInventoryIcon(Material.BEDROCK, "Click Trigger", "§8Dungeon Block", "Click to simulate a pulse."));
-		inv.setItem(12, new DBInventoryIcon(Material.SNOW_BLOCK, "Item Proximity", "§8Dungeon Block"));
-		inv.setItem(21, new DBInventoryIcon(Material.LAPIS_BLOCK, "Mob Proximity", "§8Dungeon Block"));
-		inv.setItem(30, new DBInventoryIcon(Material.REDSTONE_BLOCK, "Timer", "§8Dungeon Block"));
+		inv.setItem(3, new DBInventoryIcon(ClickTrigger.class));
+		//inv.setItem(12, new DBInventoryIcon(Material.SNOW_BLOCK, "Item Proximity", "§8Dungeon Block"));
+		//inv.setItem(21, new DBInventoryIcon(Material.LAPIS_BLOCK, "Mob Proximity", "§8Dungeon Block"));
+		//inv.setItem(30, new DBInventoryIcon(Material.REDSTONE_BLOCK, "Timer", "§8Dungeon Block"));
 		
 		//Action Blocks
-		inv.setItem(6, new DBInventoryIcon(DungeonBlock.ENTITY_SPAWNER.getMaterial(), DungeonBlock.ENTITY_SPAWNER.getName()));
-		inv.setItem(7, new DBInventoryIcon(Material.ENDER_STONE, "Entity Spawner", "§8Dungeon Block"));
-		inv.setItem(8, new DBInventoryIcon(Material.QUARTZ_ORE, "Mob Tosser", "§8Dungeon Block"));
-		inv.setItem(24, new DBInventoryIcon(Material.DIAMOND_BLOCK, "Give Item", "§8Dungeon Block"));
-		inv.setItem(25, new DBInventoryIcon(Material.WOOL, "Take Item", "§8Dungeon Block"));
-		inv.setItem(26, new DBInventoryIcon(Material.IRON_BLOCK, "Cave-In", "§8Dungeon Block"));
-		inv.setItem(33, new DBInventoryIcon(Material.GLASS, "Lightning", "§8Dungeon Block"));
-		inv.setItem(34, new DBInventoryIcon(Material.TNT, "Explosion", "§8Dungeon Block"));
-		inv.setItem(35, new DBInventoryIcon(Material.IRON_ORE, "Physics", "§8Dungeon Block"));
+		inv.setItem(6, new DBInventoryIcon(EntitySpawner.class));
+		//inv.setItem(8, new DBInventoryIcon(Material.QUARTZ_ORE, "Mob Tosser", "§8Dungeon Block"));
+		inv.setItem(24, new DBInventoryIcon(InventoryEditor.class));
+		//inv.setItem(26, new DBInventoryIcon(Material.IRON_BLOCK, "Cave-In", "§8Dungeon Block"));
+		//inv.setItem(33, new DBInventoryIcon(Material.GLASS, "Lightning", "§8Dungeon Block"));
+		//inv.setItem(34, new DBInventoryIcon(Material.TNT, "Explosion", "§8Dungeon Block"));
+		//inv.setItem(35, new DBInventoryIcon(Material.IRON_ORE, "Physics", "§8Dungeon Block"));
 	}
 
 	public static Inventory getInventory() {
@@ -57,13 +54,31 @@ public class DBInventory {
 	
 	public static class DBInventoryIcon extends ItemStack {
 
-		public DBInventoryIcon(Material mat, String name, String... tooltip) {
-			super(mat);
+		public DBInventoryIcon(Class<? extends DungeonBlock> clazz) {
+			super(Material.DIRT);
+			DungeonBlockInfo dbi = clazz.getAnnotation(DungeonBlockInfo.class);
+			if (dbi != null) {
+				ItemMeta im = this.getItemMeta();
+				im.setDisplayName(dbi.name());
+				im.setLore(Lists.newArrayList(dbi.description()));
+				this.setItemMeta(im);
+				Material mat = Material.matchMaterial(dbi.material());
+				if (mat != null) {
+					this.setType(mat);
+				}
+			}
 			
-			ItemMeta im = this.getItemMeta();
-			im.setDisplayName(name);
-			im.setLore(Lists.newArrayList(tooltip));
-			this.setItemMeta(im);
+			DungeonToolInfo dti = clazz.getAnnotation(DungeonToolInfo.class);
+			if (dti != null) {
+				ItemMeta im = this.getItemMeta();
+				im.setDisplayName(dti.name());
+				im.setLore(Lists.newArrayList(dti.description()));
+				this.setItemMeta(im);
+				Material mat = Material.matchMaterial(dti.material());
+				if (mat != null) {
+					this.setType(mat);
+				}
+			}
 		}
 	}
 }
---
+++ src/com/tidesofwaronline/Exodus/DungeonBlocks/InfoTool.java
@@ -0,0 +1,6 @@
+package com.tidesofwaronline.Exodus.DungeonBlocks;
+
+@DungeonToolInfo(description = { "Displays info on a block.", "Left click for block info.", "Right click to Edit a block.", "Shift-Left Click to select a block.", "Shift-Right Click to link a block." }, material = "BLAZE_ROD", name = "Info Tool")
+public class InfoTool extends DungeonBlock {
+
+}
