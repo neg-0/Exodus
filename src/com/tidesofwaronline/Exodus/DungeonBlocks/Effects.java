83905045d581f0f4de97bb01585ffa47ea78419f
TidesOfWarOnline <mastac@gmail.com>
2013-09-21 04:53:59 -0500

    Removed Uneeded Classes

--- src/com/tidesofwaronline/Exodus/DungeonBlocks/Effects.java
+++
@@ -1,8 +0,0 @@
-package com.tidesofwaronline.Exodus.DungeonBlocks;
-
-import com.tidesofwaronline.Exodus.DungeonBlocks.DungeonBlock.DungeonBlockInfo;
-
-@DungeonBlockInfo(description = "", hasInput = false, hasOutput = false, material = "", name = "")
-public class Effects {
-
-}
