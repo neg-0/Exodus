54cbe7b5603466a0dd774632820229b4dec55556
TidesOfWarOnline <mastac@gmail.com>
2013-09-21 04:57:58 -0500

    Added gifts

--- src/com/tidesofwaronline/Exodus/DataStructure.java
+++ src/com/tidesofwaronline/Exodus/DataStructure.java
@@ -3,6 +3,8 @@ package com.tidesofwaronline.Exodus;
 public class DataStructure {
 
 	private static String configFile = "plugins/Exodus/config.yml";
+	private static String giftsFile = "plugins/Exodus/players/gifts.yml";
+	
 	private static String pluginFolder = "plugins/Exodus/";
 
 	private static String guildsFolder = "plugins/Exodus/guilds/";
@@ -49,40 +51,8 @@ public class DataStructure {
 	public static String getXMLFile() {
 		return XMLFile;
 	}
-
-	public static void setConfigFile(String configFile) {
-		DataStructure.configFile = configFile;
-	}
-
-	public static void setGuildsFolder(String guildsFolder) {
-		DataStructure.guildsFolder = guildsFolder;
-	}
-
-	public static void setInstancesFolder(String instancesFolder) {
-		DataStructure.instancesFolder = instancesFolder;
-	}
-
-	public static void setPlayerConfigFolder(String playerConfigFolder) {
-		DataStructure.playerConfigFolder = playerConfigFolder;
-	}
-
-	public static void setPluginFolder(String pluginFolder) {
-		DataStructure.pluginFolder = pluginFolder;
-	}
-
-	public static void setSchematicsFolder(String schematicsFolder) {
-		DataStructure.schematicsFolder = schematicsFolder;
-	}
-
-	public static void setSpawnersFolder(String spawnersFolder) {
-		DataStructure.spawnersFolder = spawnersFolder;
-	}
-
-	public static void setWorldsFolder(String worldsFolder) {
-		DataStructure.worldsFolder = worldsFolder;
-	}
-
-	public static void setXMLFile(String xMLFile) {
-		XMLFile = xMLFile;
+	
+	public static String getGiftsFile() {
+		return giftsFile;
 	}
 }
