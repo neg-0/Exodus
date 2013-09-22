cb001b9ce01fdef16416498fa39e021991dc1ebe
TidesOfWarOnline <mastac@gmail.com>
2013-09-22 06:20:16 -0500

    Added explosion and lightning

--- src/com/tidesofwaronline/Exodus/DungeonBlocks/EffectsPlayer.java
+++ src/com/tidesofwaronline/Exodus/DungeonBlocks/EffectsPlayer.java
@@ -1,8 +1,298 @@
 package com.tidesofwaronline.Exodus.DungeonBlocks;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.WordUtils;
+import org.bukkit.Effect;
+import org.bukkit.Location;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+import com.tidesofwaronline.Exodus.Commands.CommandPackage;
 import com.tidesofwaronline.Exodus.DungeonBlocks.DungeonBlock.DungeonBlockInfo;
+import com.tidesofwaronline.Exodus.Util.SerializableLocation;
 
-@DungeonBlockInfo(description = "", hasInput = false, hasOutput = false, material = "", name = "")
-public class EffectsPlayer {
+@DungeonBlockInfo(description = "Plays an effect, explosion, or lightning.", hasInput = true, hasOutput = false, material = "IRON_BLOCK", name = "Effects Player")
+public class EffectsPlayer extends DungeonBlock {
+	
+	private List<Event> events = new ArrayList<Event>();
 
+	public EffectsPlayer() {
+		super();
+	}
+
+	public EffectsPlayer(Location loc) {
+		super(loc);
+	}
+
+	public EffectsPlayer(Map<String, Object> map) {
+		super(map);
+		
+		@SuppressWarnings("unchecked")
+		List<String> eventsList = (List<String>) map.get("Events");
+		
+		for (String s : eventsList) {
+			addEvent(parseEvent(s));
+		}
+	}
+	
+	@DungeonBlockCommand(description = "Appends an Event to the end of the list.", example = "add /say Hello; add wait 500", syntax = "add <Event>")
+	public String add(CommandPackage cp) {
+		
+			try {
+				cp.getPlayer().sendMessage(cp.getArgumentsString());
+				Class<?> clazz = Class.forName(Event.class.getName() + "$" + WordUtils.capitalize(cp.getArgs()[0]));
+				Constructor<?> con = Class.forName(clazz.getName()).getConstructors()[0];
+				CommandPackage com = new CommandPackage(cp.getPlugin(), cp.getPlayer(), cp.getExoPlayer(), Arrays.copyOfRange(cp.getArgs(), 1, cp.getArgs().length));
+				return "Added " + addEvent((Event) con.newInstance(new Event(), com));
+			} catch (IllegalArgumentException e) {
+				cp.getPlayer().sendMessage(e.getMessage());
+			} catch (SecurityException e) {
+				e.printStackTrace();
+			} catch (InstantiationException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				return "That's not a valid command.";
+			}
+			return "nope";
+	}
+	
+	public Event addEvent(Event e) {
+		this.events.add(e);
+		return e;
+	}
+	
+	@DungeonBlockCommand(description = "Clears the Event list.", example = "", syntax = "")
+	public String clear(CommandPackage cp) {
+		this.clearEvents();
+		return "Events cleared.";
+	}
+
+	public void clearEvents() {
+		events.clear();
+	}
+	
+	@DungeonBlockCommand(description = "Returns a list of available Events.", example = "", syntax = "")
+	public String events(CommandPackage cp) {
+		List<String> list = new ArrayList<String>();
+		for (Class<?> e : Event.class.getDeclaredClasses()) {
+			list.add(e.getSimpleName());
+		}
+		return Joiner.on(", ").join(list);
+	}
+
+	public List<Event> getEvents() {
+		return events;
+	}
+
+	@DungeonBlockCommand(description = "Inserts a command at the specified location.", example = "insert 4 say Hello", syntax = "insert <#> <Command>")
+	public String insert(CommandPackage cp) {
+		if (cp.getArgs().length < 2) {
+			return "Invalid number of Arguments.";
+		}
+		return null;
+	}
+
+	public Event insertEvent(Event e, int i) {
+		this.events.add(i, e);
+		return e;
+	}
+
+	@DungeonBlockCommand(description = "Returns the list of executed commands.", example = "", syntax = "")
+	public List<String> list(CommandPackage cp) {
+		List<String> toReturn = new ArrayList<String>();
+		for (int i = 0; i < this.getEvents().size(); i++) {
+			toReturn.add((i + 1) + ". §e" + this.getEvents().get(i));
+		}
+		if (toReturn.size() == 0) {
+			return Lists.newArrayList("Events list empty.");
+		} else {
+			return toReturn;
+		}
+	}
+	
+	@Override
+	public void onTrigger(DungeonBlockEvent event) {
+		boolean trigger = true;
+		for (Event e : getEvents()) {
+			if (!e.onTrigger(event)) {
+				trigger = false;
+			}
+		}
+		if (trigger == true) {
+			triggerLinkedBlocks(event);
+		}
+	}
+
+	protected Event parseEvent(String s) {
+		String command = s.split(" ")[0];
+		String[] arguments = Arrays.copyOfRange(s.split(" "), 1, s.split(" ").length);
+		try {
+			Class<?> clazz = Class.forName(Event.class.getName() + "$" + WordUtils.capitalize(command));
+			Constructor<?> con = Class.forName(clazz.getName()).getConstructors()[0];
+			Event event = new Event();
+			CommandPackage com = new CommandPackage(null, null, null, arguments);
+			return (Event) con.newInstance(new Object[] {event, com});
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			return null;
+		}
+		return null;
+	}
+	
+	@DungeonBlockCommand(description = "Removes the specified indices.", example = "remove 5 2 1", syntax = "remove <Index, Index...>")
+	public String remove(CommandPackage cp) {
+		if (cp.getArgs().length == 0) {
+			return "Invalid number of Arguments.";
+		}
+		List<Integer> toRemove = new ArrayList<Integer>();
+		for (String s : cp.getArgs()) {
+			try {
+				int i = Integer.valueOf(s);
+				events.remove(i);
+				toRemove.add(i);
+			} catch (NumberFormatException e) {}
+		}
+		if (toRemove.size() == 0) {
+			return "No indexes were found. Nothing removed.";
+		}
+		String toReturn = "Removed Command";
+		if (toRemove.size() > 1) {
+			toReturn += "s";
+		}
+		toReturn += " " + Joiner.on(", ").join(toRemove);
+		toReturn += ".";
+		return toReturn;
+	}
+
+	@Override
+	public Map<String, Object> serialize() {
+		Map<String, Object> map = new HashMap<String, Object>();
+
+		map.putAll(super.serialize());
+
+		List<String> eventsList = new ArrayList<String>();
+		for (Event e : getEvents()) {
+			eventsList.add(e.toString());
+		}
+
+		map.put("Events", eventsList);
+
+		return map;
+	}
+
+	public class Event {
+		
+		SerializableLocation location;
+		Effect effect;
+		boolean damage = true;
+		
+		public Event() {}
+		
+		public Event(CommandPackage cp) {
+			
+			//sample:
+			//add lightning safe here
+			//add explosion unsafe at 100 64 200
+			
+			
+			if (cp.getArgs()[0].equalsIgnoreCase("safe")) {
+				damage = false;
+			} else if (cp.getArgs()[0].equalsIgnoreCase("unsafe")) {
+				damage = true;
+			}
+			
+			if (cp.getArgs()[1].equalsIgnoreCase("here")) {
+				location = new SerializableLocation(cp.getPlayer().getLocation());
+			} else if (cp.getArgs()[1].equalsIgnoreCase("at")) {
+				location = SerializableLocation.fromString(cp.getPlayer().getWorld().getName() + " " + Joiner.on(" ").join(Arrays.copyOfRange(cp.getArgs(), 3, 6)));
+			}
+			
+			
+//			if (effect == null) {
+//				throw new IllegalArgumentException("That is not a valid effect.");
+//			}
+			
+			
+		}
+
+		public boolean onTrigger(DungeonBlockEvent event) {
+			return false;
+		}
+		
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder(); 
+			sb.append(this.getClass().getSimpleName());
+			sb.append(" ");
+			if (damage) {
+				sb.append("unsafe");
+			} else {
+				sb.append("safe");
+			}
+			sb.append(" at ");
+			sb.append(location.getWorldName());
+			sb.append(" ");
+			sb.append(location.getX());
+			sb.append(" ");
+			sb.append(location.getY());
+			sb.append(" ");
+			sb.append(location.getZ());
+			return sb.toString();
+		}
+		
+		public class Lightning extends Event {
+			
+			public Lightning(CommandPackage cp) {
+				super(cp);
+			}
+			
+			@Override
+			public boolean onTrigger(DungeonBlockEvent event) {
+				if (damage) {
+					location.getWorld().strikeLightning(location.toLocation());
+				} else {
+					location.getWorld().strikeLightningEffect(location.toLocation());
+				}
+				return true;
+			}
+		}
+		
+		public class Explosion extends Event {
+			
+			public Explosion(CommandPackage cp) {
+				super(cp);
+			}
+			
+			@Override
+			public boolean onTrigger(DungeonBlockEvent event) {
+				if (damage) {
+					location.getWorld().createExplosion(location.toLocation(), 4.0F, damage);
+				} else {
+					location.getWorld().createExplosion(location.toLocation(), 0.0F, damage);
+				}
+				return true;
+			}
+		}
+
+	}
 }
