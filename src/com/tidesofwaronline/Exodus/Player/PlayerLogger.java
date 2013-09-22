99c87f78d54bfb93f7ecff5fc059cef0bac6ca97
TidesOfWarOnline <mastac@gmail.com>
2013-09-22 01:24:13 -0500

    Added DatabaseUpdater

--- src/com/tidesofwaronline/Exodus/Player/PlayerLogger.java
+++ src/com/tidesofwaronline/Exodus/Player/PlayerLogger.java
@@ -1,16 +1,24 @@
 package com.tidesofwaronline.Exodus.Player;
 
 import java.sql.Connection;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 
 import com.tidesofwaronline.Exodus.Exodus;
 import com.tidesofwaronline.Exodus.Database.mysql.MySQL;
 
 
-public class PlayerLogger {
+public class PlayerLogger implements Listener{
 	
 	Exodus exodus;
 	MySQL mysql;
 	Connection c;
+	
+	Queue<Event> queue = new LinkedList<Event>();
 	
 	public PlayerLogger(Exodus exodus) {
 		this.exodus = exodus;
@@ -19,5 +27,37 @@ public class PlayerLogger {
 	
 	void openConnection() {
 	    c = mysql.openConnection();
+	}
+	
+	@EventHandler
+	void logBlockBreak(BlockBreakEvent e) {
+		queue.add(new Event(e.getPlayer().getName(), String.valueOf(e.getBlock().getTypeId())));
+	}
+	
+	void UpdateEntry(Event e) {
+		
+	}
+	
+	final class Event {
+		
+		final String key, value;
+		
+		Event(final String key, final String value) {
+			this.key = key;
+			this.value = value;
+		}
+	}
+	
+	class DatabaseUpdater implements Runnable {
+		
+		boolean active = true;
+
+		@Override
+		public void run() {
+			while(active) {
+				queue.remove();
+			}
+		}
+		
 	}
 }
